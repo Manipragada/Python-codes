b194772d5e6016b53ee300439e655f1599bd7880
Krishna-git9 <Krishna.kvali@gmail.com>
2025-07-02 18:29:07 +0530

    changes in 4 and 5

--- Interview/Ques4.py
+++ Interview/Ques4.py
@@ -4,3 +4,4 @@
 # Output
 # (4,5) (3,6)
 # what change i have done
+#ok
--- Interview/Ques5.py
+++ Interview/Ques5.py
@@ -1,2 +1,3 @@
 # lst = List(1,2,3,4,5,6)
 # get even from list and sum of squares of even numbers in python
+# I havea error in this line
